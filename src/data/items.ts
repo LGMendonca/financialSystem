a4cd284df6c799d0068b17e7ee960383a9dacd50
LGMendonca <lmendoncadm@gmail.com>
2023-08-22 18:06:21 -0300

    first commit

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import {Item} from '../types/Item';
+
+export const items: Item[] = [
+  {date: new Date(2022, 7, 6), category: 'expense', title: 'McDonalds', value: 32.12 },
+  {date: new Date(2022, 7, 15), category: 'expense', title: 'Burger King', value: 28 },
+  {date: new Date(2022, 7, 16), category: 'expense', title: 'Aluguel Apt', value: 2300 },
+  {date: new Date(2022, 7, 18), category: 'salary', title: 'Salário ACME', value: 4500 },
+]
